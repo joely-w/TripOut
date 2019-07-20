06b4dcece0b638625626706178ace853fbd54f64
Joel Winterton <joely-w@users.noreply.github.com>
2019-07-20 23:33:40 +0100

    Initial project

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 <body>
 <?php include('navigation.php'); ?>
 <div class="hero">
-    <h1></h1>
+    <ul>
+        <li><h1>Scratch Conference Europe</h1></li>
+    </ul>
+    <div class="dropdown">
+        <form class="login" role="login" method="post" action="authenticate.php">
+            <input class="form-control" type="text" placeholder="Email" name="email" required autocomplete="off">
+            <input class="form-control" type="password" placeholder="Password" name="password" required
+                   autocomplete="off">
+            <button>Login</button>
+        </form>
+    </div>
 </div>
 </body>
