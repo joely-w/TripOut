2beeaca443f771e66a41e2bf0f0bc92e98ec0d5e
Joel Winterton <joely-w@users.noreply.github.com>
2019-09-14 23:15:37 +0100

    Sample event columns added

--- index.php
+++ index.php
@@ -32,54 +32,25 @@ include('header.php'); ?>
         <div class="arrow-right"></div>
     </div>
 </div>
-<h1>It's a crazy thing called batman</h1>
-<div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Enim nunc faucibus a pellentesque sit amet porttitor eget. Magna ac placerat vestibulum lectus
-        mauris. Placerat in egestas erat imperdiet. Commodo odio aenean sed adipiscing diam donec. Eget nunc scelerisque
-        viverra mauris. Pulvinar neque laoreet suspendisse interdum consectetur libero id. Tempor nec feugiat nisl
-        pretium. Nec ultrices dui sapien eget. Purus ut faucibus pulvinar elementum integer enim neque volutpat ac.
-        Posuere morbi leo urna molestie at elementum eu facilisis. Sed odio morbi quis commodo odio aenean sed. Nulla
-        facilisi nullam vehicula ipsum a arcu. Fringilla ut morbi tincidunt augue interdum velit euismod in
-        pellentesque. Bibendum enim facilisis gravida neque convallis. Vestibulum morbi blandit cursus risus at. Duis ut
-        diam quam nulla porttitor massa id neque aliquam. Et netus et malesuada fames ac turpis egestas sed. Dolor morbi
-        non arcu risus quis.
+<div class="container events">
+    <h1>Events you might be interested in</h1>
 
-        Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Eget mauris pharetra et ultrices neque.
-        Dolor sit amet consectetur adipiscing elit ut aliquam. Varius duis at consectetur lorem donec massa sapien.
-        Consequat ac felis donec et. Morbi tempus iaculis urna id volutpat. Purus semper eget duis at tellus at urna.
-        Sit amet justo donec enim diam vulputate ut pharetra sit. Neque vitae tempus quam pellentesque nec nam aliquam
-        sem et. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Sapien eget mi proin sed libero enim.
-        Tellus mauris a diam maecenas sed. Etiam tempor orci eu lobortis elementum nibh.
+    <div class="row">
+        <div class="col-md-3 event-thumb"><img src="https://source.unsplash.com/random/400x400"/>
+            <h3>Chatterley Whitfield Colliery Heritage Day</h3><span>Make sure your family avoid the mines though, mines are pretty dangerous things. But do come along, it'll be a lark, plus even if it's not it fills my coursework text filler so what ho!</span>
+        </div>
+        <div class="col-md-3 event-thumb"><img src="https://source.unsplash.com/random/500x500"/>
+            <h3>Bonfire, lantern walk and fireworks</h3><span>Make sure your family avoid the mines though, mines are pretty dangerous things. But do come along, it'll be a lark, plus even if it's not it fills my coursework text filler so what ho!</span>
+        </div>
+        <div class="col-md-3 event-thumb"><img src="https://source.unsplash.com/random/200x200"/>
+            <h3>NCS Graduation Event Stoke</h3><span>Make sure your family avoid the mines though, mines are pretty dangerous things. But do come along, it'll be a lark, plus even if it's not it fills my coursework text filler so what ho!</span>
+        </div>
+        <div class="col-md-3 event-thumb"><img src="https://source.unsplash.com/random/200x201"/>
+            <h3>Greatest Showman Drive-In Screening</h3><span>Make sure your family avoid the mines though, mines are pretty dangerous things. But do come along, it'll be a lark, plus even if it's not it fills my coursework text filler so what ho!</span>
+        </div>
 
-        Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. A erat nam at lectus urna duis
-        convallis convallis tellus. Magna fermentum iaculis eu non diam phasellus vestibulum lorem. Condimentum lacinia
-        quis vel eros donec ac odio tempor orci. Viverra tellus in hac habitasse. Sit amet aliquam id diam maecenas
-        ultricies. Ultrices neque ornare aenean euismod elementum nisi quis. Dapibus ultrices in iaculis nunc sed augue.
-        Et magnis dis parturient montes nascetur ridiculus. Vitae et leo duis ut diam quam nulla. Nec ullamcorper sit
-        amet risus nullam eget. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Libero id
-        faucibus nisl tincidunt eget nullam non. Adipiscing at in tellus integer. Ut sem viverra aliquet eget sit.
-        Egestas sed tempus urna et pharetra. Sit amet tellus cras adipiscing enim eu turpis. Morbi tincidunt ornare
-        massa eget egestas purus viverra. Nec ultrices dui sapien eget mi proin.
+    </div>
 
-        Turpis in eu mi bibendum. Lorem ipsum dolor sit amet consectetur adipiscing elit ut. Nulla pharetra diam sit
-        amet nisl suscipit adipiscing bibendum. Eget gravida cum sociis natoque penatibus et magnis dis. Morbi tempus
-        iaculis urna id volutpat lacus laoreet non. Quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor
-        sit. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Accumsan tortor posuere ac ut
-        consequat. Gravida dictum fusce ut placerat orci nulla pellentesque dignissim. Sed felis eget velit aliquet.
-        Placerat duis ultricies lacus sed turpis tincidunt id aliquet risus. Sem nulla pharetra diam sit. Massa massa
-        ultricies mi quis hendrerit dolor magna eget est. Velit scelerisque in dictum non consectetur a erat nam at. Sed
-        elementum tempus egestas sed sed risus. Egestas integer eget aliquet nibh praesent tristique magna sit.
-        Adipiscing at in tellus integer feugiat scelerisque. Sapien et ligula ullamcorper malesuada. Molestie at
-        elementum eu facilisis sed odio morbi quis. Risus viverra adipiscing at in tellus integer feugiat.
-
-        Commodo odio aenean sed adipiscing diam. Enim praesent elementum facilisis leo. Nullam eget felis eget nunc
-        lobortis mattis aliquam. Ornare suspendisse sed nisi lacus sed viverra. Scelerisque in dictum non consectetur a
-        erat. Id venenatis a condimentum vitae sapien pellentesque. Velit egestas dui id ornare arcu odio. Morbi blandit
-        cursus risus at ultrices mi. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Egestas
-        egestas fringilla phasellus faucibus scelerisque. Id interdum velit laoreet id donec. Fermentum leo vel orci
-        porta non pulvinar neque laoreet suspendisse. Tincidunt eget nullam non nisi est sit. Id nibh tortor id aliquet
-        lectus proin nibh nisl.</p>
 </div>
 <?php include('footer.php'); ?>
 </body>
