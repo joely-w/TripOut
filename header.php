6f06d15072b9eda96045aaec8fd5aa55c01c23b1
Joel Winterton <joely-w@users.noreply.github.com>
2019-09-14 23:15:19 +0100

    Custom scripts to be included after all other scripts to allow jQuery usage

--- header.php
+++ header.php
@@ -2,6 +2,12 @@
 session_start(); #Start session on all pages before headers are sent
 ?>
 <head>
+    <title>Trip Out - <?php echo $title ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/stylesheets/style.css">
+    <script src="/account/login.js"></script>
     <? if (isset($scripts)) { #If any unique scripts are required, set in pages $script array.
         foreach ($scripts as $path) {
             ?>
@@ -9,10 +15,4 @@ session_start(); #Start session on all pages before headers are sent
             <?
         }
     } ?>
-    <title>Trip Out - <?php echo $title ?></title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="/stylesheets/style.css">
-    <script src="/account/login.js"></script>
 </head>
