1652d3649b8105204f4d3eab9da9b4d2eb87defd
Joel Winterton <joely-w@users.noreply.github.com>
2019-09-21 00:35:52 +0100

    Now contains modal for My images

--- account/manage.php
+++ account/manage.php
@@ -16,73 +16,79 @@ $Image = new myImages();
         <!--Content updated if error occurs!-->
         <?php
         if (isset($_SESSION['Username'])) {
+        ?>
+        <h2>My account
+        </h2>
+        <?
+        $fields = $account->AccountFields();
+        foreach ($fields as $field) {
+            if ($field["Viewable"] == true) {#If actual value of field should not be view by user, hide it (for instance password)
+                $valueAttribute = $_SESSION[$field['UserField']];
+            } else {
+                $valueAttribute = "Hidden value";
+            }
             ?>
-            <h2>My account
-            </h2>
+            <div class="col-md-4">
+                <label>
+                    <?php echo $field['UserField'] ?>:
+                    <br>
+                    <input type="<?php echo $field['Datatype'] ?>" name="<?php echo $field['UserField'] ?>"
+                           value="<?php echo $valueAttribute ?>"
+                           onchange="updateValue('<?php echo $field['UserField'] ?>',this.value)"
+                           class="form-control"/>
+                </label>
+            </div>
             <?
-            $fields = $account->AccountFields();
-            foreach ($fields as $field) {
-                if ($field["Viewable"] == true) {#If actual value of field should not be view by user, hide it (for instance password)
-                    $valueAttribute = $_SESSION[$field['UserField']];
-                } else {
-                    $valueAttribute = "Hidden value";
-                }
+        } ?>
+        <button type="button" onclick="LogOut()" class="btn btn-primary">Logout
+        </button>
+        <h2>My images
+        </h2>
+        <div class="image-upload row">
+            <?php
+            $images_arr = $Image->DisplayImages($_SESSION['Username']);
+            foreach ($images_arr as $image) {#Structure: Filename, Filetype
+                $path = "/events/images/" . $_SESSION['Username'] . "/" . $image['Filename'] . "." . $image['Filetype'];
                 ?>
-                <div class="col-md-4">
-                    <label>
-                        <?php echo $field['UserField'] ?>:
-                        <br>
-                        <input type="<?php echo $field['Datatype'] ?>" name="<?php echo $field['UserField'] ?>"
-                               value="<?php echo $valueAttribute ?>"
-                               onchange="updateValue('<?php echo $field['UserField'] ?>',this.value)"
-                               class="form-control"/>
-                    </label>
+                <div class="col-md-2 img-thumb">
+                    <a onclick="Modal('<?php echo $path?>')"><img src="<?php echo $path; ?>"/></a>
                 </div>
                 <?
-            } ?>
-            <button type="button" onclick="LogOut()" class="btn btn-primary">Logout
-            </button>
-            <h2>My images
-            </h2>
-            <div class="image-upload row">
-                <?php
-                $images_arr = $Image->DisplayImages($_SESSION['Username']);
-                foreach ($images_arr as $image) {#Structure: Filename, Filetype
-                    $path = "/events/images/" . $_SESSION['Username'] . "/" . $image['Filename'] . "." . $image['Filetype'];
-                    ?>
-                    <div class="col-md-2">
-                        <a target="_blank" href="/serve/images.php?image=<?php echo $path ?>"><img class="thumbnail"
-                                                                                                   src="<?php echo $path; ?>"/></a>
-                    </div>
-                    <?
-                }
-                ?>
-            </div>
-            <h2>Upload image</h2>
-            <div class="image-upload row">
-                <form id="uploadimage" action="/images/upload_image_process.php" method="post" enctype="multipart/form-data">
-                    <div id="image_preview"><img id="previewing" height="100" src="https://icon-library.net/images/file-icon-png/file-icon-png-23.jpg" /></div>
-                    <div id="selectImage">
-                        <label>Select Your Image</label><br/>
-                        <input type="file" name="file" id="file" required />
-                        <input type="submit" value="Upload" class="submit" />
-                    </div>
-                </form>
-            </div>
-        <h4 id='loading' >loading..</h4>
-            </div>
-
-            <?
-        } else {
+            }
             ?>
-            <div class="alert alert-danger" role="alert">
-                You're not logged in!
-                <a href="login.php">Login now
-                </a>
-            </div>
-            <?
-        }
-        ?>
+        </div>
+        <h2>Upload image</h2>
+        <div class="image-upload row">
+            <form id="uploadimage" action="/images/upload_image_process.php" method="post"
+                  enctype="multipart/form-data">
+                <div id="image_preview"><img id="previewing" height="100"
+                                             src="https://icon-library.net/images/file-icon-png/file-icon-png-23.jpg"/>
+                </div>
+                <div id="selectImage">
+                    <label>Select Your Image</label><br/>
+                    <input type="file" name="file" id="file" required/>
+                    <input type="submit" value="Upload" class="submit"/>
+                </div>
+            </form>
+        </div>
+        <div class="modal" id="modal">
+            <a onclick="closeModal()" class="close">&#10006;</a>
+            <img src="" id="modalImage" />
+        </div>
+
     </div>
+
+    <?
+    } else {
+        ?>
+        <div class="alert alert-danger" role="alert">
+            You're not logged in!
+            <a href="login.php">Login now
+            </a>
+        </div>
+        <?
+    }
+    ?>
+</div>
 </div>
 </body>
