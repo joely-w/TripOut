198af4487efe76f27765dfab361242e9b8c3f201
Joel Winterton <joely-w@users.noreply.github.com>
2019-09-24 10:18:11 +0100

    Image options migrated to upload_handler.js Script now only sends updated values to backend and logs user out.

--- account/manage.js
+++ account/manage.js
@@ -1,48 +1,41 @@
 function updateValue(field, value) { /*Call the update method in the API*/
-	console.log(field,value);
-	data = ["update", field, value];
-	$.ajax({
-		type: "POST",
-		url: '/account/manage_process.php',
-		data: {
-			data: data
-		},
-		success: function(response) {
-			var jsonData = JSON.parse(response);
-			if (jsonData.success === 1) {
-				$("#report").html("<div class='alert alert-success' role='alert'> " + field + " has been updated!</div>");
-				setTimeout(function() {
-					document.getElementById("report").innerHTML = "";
-				}, 3000);
-			} else {
-				document.getElementById("report").innerHTML = "<span class='error'>" + jsonData.error + "</span>";
-			}
-		}
-	});
+    console.log(field, value);
+    data = ["update", field, value];
+    $.ajax({
+        type: "POST",
+        url: '/account/manage_process.php',
+        data: {
+            data: data
+        },
+        success: function (response) {
+            console.log(response);
+            var jsonData = JSON.parse(response);
+            if (jsonData.success === 1) {
+                $("#report").html("<div class='alert alert-success' role='alert'> " + field + " has been updated!</div>");
+                setTimeout(function () {
+                    document.getElementById("report").innerHTML = "";
+                }, 3000);
+            } else {
+                document.getElementById("report").innerHTML = "<span class='error'>" + jsonData.error + "</span>";
+            }
+        }
+    });
 }
-
 
 function LogOut() { /*Call the logout method in the API*/
-	data = ["logout"];
-	$.ajax({
-		type: "POST",
-		url: '/account/manage_process.php',
-		data: {
-			data: data
-		},
-		success: function(response) {
-			var jsonData = JSON.parse(response);
-			if (jsonData.logout === 1) {
-				location.reload()
-			}
-		}
-	});
+    data = ["logout"];
+    $.ajax({
+        type: "POST",
+        url: '/account/manage_process.php',
+        data: {
+            data: data
+        },
+        success: function (response) {
+            var jsonData = JSON.parse(response);
+            if (jsonData.logout === 1) {
+                location.reload()
+            }
+        }
+    });
 }
-function Modal(path){
-	document.getElementById("modalImage").src=path;
-	document.getElementById("modal").style.display="block";
-}
-function closeModal(){
-	document.getElementById("modal").style.display="none";
 
-}
