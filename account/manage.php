6196fb1247585197e2ccb02fad835cca37555aed
Joel Winterton <joely-w@users.noreply.github.com>
2019-09-24 10:15:58 +0100

    Image upload can be previewed before uploading. Checks for image format at frontend as well as backend to reduce redundant uploads to server. Includes file upload script for uploading images separately.

--- account/manage.php
+++ account/manage.php
@@ -1,12 +1,13 @@
 <?php
 $title = "Manage My Account";
-$scripts = array("manage.js");
+$scripts = array("manage.js", "../images/upload_handler.js");
 include('../header.php');
 include('../database/config.php');
 $account = new Login();
 $Image = new myImages();
 #If a user field does not appear on this page, it probably isn't in the LinkedEditable table, add it!
 ?>
+
 <body>
 <?php include('../navigation.php'); ?>
 <div class="container">
@@ -19,9 +20,14 @@ $Image = new myImages();
         ?>
         <h2>My account
         </h2>
-        <?
-        $fields = $account->AccountFields();
-        foreach ($fields as $field) {
+        <form>
+            <input type="text" style="display:none">
+            <input type="password" style="display:none">
+            <?
+            $fields = $account->AccountFields();
+            foreach ($fields
+
+            as $field) {
             if ($field["Viewable"] == true) {#If actual value of field should not be view by user, hide it (for instance password)
                 $valueAttribute = $_SESSION[$field['UserField']];
             } else {
@@ -38,42 +44,46 @@ $Image = new myImages();
                            class="form-control"/>
                 </label>
             </div>
-            <?
-        } ?>
+        </form>
+    <?
+    } ?>
         <button type="button" onclick="LogOut()" class="btn btn-primary">Logout
         </button>
         <h2>My images
         </h2>
-        <div class="image-upload row">
+        <div id="images" class="image-upload row">
             <?php
             $images_arr = $Image->DisplayImages($_SESSION['Username']);
             foreach ($images_arr as $image) {#Structure: Filename, Filetype
                 $path = "/events/images/" . $_SESSION['Username'] . "/" . $image['Filename'] . "." . $image['Filetype'];
                 ?>
                 <div class="col-md-2 img-thumb">
-                    <a onclick="Modal('<?php echo $path?>')"><img src="<?php echo $path; ?>"/></a>
+                    <a onclick="Modal('<?php echo $path ?>')"><img src="<?php echo $path; ?>"/></a>
                 </div>
                 <?
             }
             ?>
         </div>
         <h2>Upload image</h2>
-        <div class="image-upload row">
-            <form id="uploadimage" action="/images/upload_image_process.php" method="post"
-                  enctype="multipart/form-data">
-                <div id="image_preview"><img id="previewing" height="100"
-                                             src="https://icon-library.net/images/file-icon-png/file-icon-png-23.jpg"/>
-                </div>
+        <div id="upload-notifications"></div>
+        <!--Append any notifications about status of upload to this div-->
+        <div id="image-upload" class="image-upload row">
+            <form id="uploadimage" method="post" enctype="multipart/form-data">
                 <div id="selectImage">
-                    <label>Select Your Image</label><br/>
-                    <input type="file" name="file" id="file" required/>
-                    <input type="submit" value="Upload" class="submit"/>
+                    <label class="uploader btn btn-default">
+                        <img id="previewing" height="50"
+                             src="https://icon-library.net/images/file-icon-png/file-icon-png-23.jpg"/>
+                        <label>Select Your Image</label>
+                        <input class="btn" onchange="fileHandler(this)" type="file" name="file" id="file" required/>
+                    </label>
+
+                    <input type="submit" value="Upload" class="submit btn btn-primary"/>
                 </div>
             </form>
         </div>
         <div class="modal" id="modal">
             <a onclick="closeModal()" class="close">&#10006;</a>
-            <img src="" id="modalImage" />
+            <img src="" id="modalImage"/>
         </div>
 
     </div>
