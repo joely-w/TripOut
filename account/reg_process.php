a629b209ba928db0eb4dd2ed07ed6a46f65f2f67
Joel Winterton <joely-w@users.noreply.github.com>
2019-10-15 09:55:40 +0100

    Passwords salted before hashed

--- account/reg_process.php
+++ account/reg_process.php
@@ -7,16 +7,24 @@ if ($database->NotEmpty($_POST, ["name", "email", "username", "password"])) { #C
     $username = $database->Escape($_POST['username']);
     $name = $database->Escape($_POST['name']);
     $email = $database->Escape($_POST['email']);
-    $password = md5($_POST['password']);
     #Check if email is in correct format and check if email already exists
-    if ($database->Email($email) and !($database->UserExists($username))) {
-        mkdir("/var/www/html/events/images/$username");
-        $query = "INSERT INTO Users (Username, Password,Email,Fullname,Reputation) VALUES ('$username','$password','$email','$name', 0)";
+    if ($database->Email($email)) {
+        if (!($database->UserExists($username, "Username"))) {
+            $salt = $database->GenerateSalt();
+            $password = md5($_POST['password'] . $salt);
+
+            $query = "INSERT INTO Users (Username, Password,Email,Fullname,Reputation, Salt) VALUES ('$username','$password','$email','$name', 0, '$salt')";
         $result = $database->Execute($query);#Register user in database
+        } else {
+            $database->errors[] = "User already exists!";
+        }
+    } else {
+        $database->errors[] = "Email already exists";
     }
 }
 
 if ($result == true) { #Report back if the registration has succeeded
+    mkdir("/var/www/html/events/images/$username"); #If user inserted into database create folder
     echo json_encode(array('success' => 1));
 } else {
     echo json_encode(array('success' => 0, 'errors' => $database->errors));
