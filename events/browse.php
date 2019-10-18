35d255e50edbd6c9dcbb818b3d2d5c59eb768387
Joel Winterton <joely-w@users.noreply.github.com>
2019-10-19 00:24:03 +0100

    Page includes script to display events

---
+++ events/browse.php
@@ -0,0 +1,17 @@
+<?php
+$title = "Home";
+$scripts = array("browse.js");
+include('../header.php'); ?>
+<body>
+<?php include('../navigation.php'); ?>
+<div class="container events">
+    <h1>Browse events</h1>
+
+    <div id="events" class="row">
+
+    </div>
+
+</div>
+<?php
+include('../footer.php'); ?>
+</body>
