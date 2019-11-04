94303e541af40e1057491f7f4d934bf0f801ae6c
Joel Winterton <joely-w@users.noreply.github.com>
2019-11-04 23:11:07 +0000

    Now appends linked thumbnails

--- events/browse.js
+++ events/browse.js
@@ -12,7 +12,7 @@ function Browse() { /*Grab all events from API*/
 }
 
 function appendEvent(event_details) {
-    let event = `<div class="col-md-4 event-thumb"><img src="${event_details['thumbnail']}" alt="image"/><h3>${event_details['title']}</h3><span>${event_details['description']}</span></div>`;
+    let event = `<div class="col-md-4 event-thumb"><a href="./event.php?id=${event_details['id']}"><img src="${event_details['thumbnail']}" alt="image"/><h3>${event_details['title']}</h3><span>${event_details['description']}</span></a></div>`;
     $("#events").append(event);
 }
 
