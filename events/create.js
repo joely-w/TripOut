4187d5d3364132d8ac7c2a9cbed0b41dd5768367
Joel Winterton <joely-w@users.noreply.github.com>
2019-11-17 00:14:38 +0000

    Handles occurence and posts to backend

--- events/create.js
+++ events/create.js
@@ -14,7 +14,6 @@ let toolbar_elements = [ /*Contains all element details for text sidebar, used t
 
 function ImageAppender(array_index, data_value) {
     let image_array = contentFields[array_index].dataSrc;
-    console.log(image_array);
     /* If image already in upload section, remove. Otherwise add image to sections selected images*/
     const index = image_array.indexOf(data_value);
     if (index > -1) {
@@ -25,10 +24,11 @@ function ImageAppender(array_index, data_value) {
 }
 
 function Add(content) {
+    let container;
     if (content === "image") {
-        var container = document.createElement("div");
+        container = document.createElement("div");
         container.className = "content-el";
-        container.id = number_of_fields;
+        container.id = (number_of_fields).toString();
         container.className = "image-upload select-image row";
         $.ajax({
             url: "/images/myImages.php",
@@ -47,7 +47,7 @@ function Add(content) {
     }
     if (content === "text") {
         /*Create sidebar for editing text*/
-        var container = document.createElement("div");
+        container = document.createElement("div");
         container.className = "content-el";
         // noinspection JSValidateTypes
         container.id = number_of_fields;
@@ -86,10 +86,29 @@ function Add(content) {
     ));
 }
 
-$("#event_form").submit(function (e) {
-    e.preventDefault();
-    processForm();
-});
+function parseOccurence() {
+    let starttime = $("#datetimepicker").data('date');
+    let endtime = $("#datetimepicker1").data('date');
+
+    if (current_recurrence === "once") {
+        let startdate = $('#startdate').data('date');
+        let enddate = $('#enddate').data('date');
+        return {type: "once", startdate: startdate, enddate: enddate, starttime: starttime, endtime: endtime}
+    } else if (current_recurrence === "daily") {
+        return {type: "daily", starttime: starttime, endtime: endtime}
+    } else if (current_recurrence === "weekly") {
+        let day = $('#day').val();
+        return {type: "weekly", starttime: starttime, endtime: endtime, day: day}
+    } else if (current_recurrence === "monthly") {
+        let week = $('#week').val();
+        let day = $('#day').val();
+        return {type: "monthly", starttime: starttime, endtime: endtime, week: week, day: day}
+    } else if (current_recurrence === "yearly") {
+        let day = $('#day').val();
+        let month = $('#month').val();
+        return {type: "yearly", starttime: starttime, endtime: endtime, day: day, month: month}
+    }
+}
 
 function processForm() {
     for (let i = 0; i < contentFields.length; i++) {
@@ -97,14 +116,122 @@ function processForm() {
             contentFields[i].dataSrc = document.getElementById(contentFields[i].dataSrc).innerHTML; /*In processForm() as should not be called before user has finished editing and is submitting*/
         }
     }
+    console.log({
+        eventOccurence: parseOccurence(),
+        eventTitle: document.getElementById("title").value,
+        content: contentFields
+    });
     $.ajax({
         url: "/events/create_process.php",
         type: "POST",
-        data: {eventTitle: document.getElementById("title").value, content: contentFields},
+        data: {
+            eventOccurence: parseOccurence(),
+            eventTitle: document.getElementById("title").value,
+            content: contentFields
+        },
         success: function (response) {
             console.log(response);
-            $('#create').innerHTML = '<h1>Form has been submitted!</h1>';
         },
 
     });
 }
+
+let current_post;
+
+function postCodeLookup(postcode) {
+    if (postcode.length >= 6 && current_post !== postcode) { //If postcode right length and has changed from previous lookup
+        $.ajax({
+            url: "https://api.postcodes.io/postcodes/" + postcode,
+            type: 'GET',
+            success: function (res) {
+                console.log(res);
+                if (res['status'] === 200) {
+                    $("#county").val(res['result']['parish']);
+                    $("#county").prop("disabled", true);
+                    current_post = postcode;
+                    showMap(10, postcode)
+                }
+            }
+        });
+    }
+}
+
+let image_exist = false;
+
+function showMap(zoom, mode) {
+    let address;
+    let marker;
+    let res = "500x300"; //Resolution for map image
+    if (mode === "addr") {
+        if (current_post.length >= 6 && image_exist === false) {
+            address = encodeURI($("#houseno").val() + $("#streetname").val() + "," + $("#county").val() + "," + current_post);
+            marker = encodeURI("size:mid|color:0xFFFF00|label:C|" + address);
+            let map_resource = `https://maps.googleapis.com/maps/api/staticmap?center=${address}&size=${res}&key=AIzaSyAqt8ejRfMThaP6C3Kfxcd8fN7OpI5RXUc&zoom=${zoom}&markers=${marker}`;
+            $("#map").html(`<img src="${map_resource}" />`);
+        }
+    } else {
+        address = encodeURI(current_post);
+        marker = encodeURI("size:mid|color:0xFFFF00|label:C|" + address);
+        let map_resource = `https://maps.googleapis.com/maps/api/staticmap?center=${address}&size=${res}&key=AIzaSyAqt8ejRfMThaP6C3Kfxcd8fN7OpI5RXUc&zoom=${zoom}&markers=${marker}`;
+        $("#map").html(`<img src="${map_resource}" />`);
+    }
+
+}
+
+let current_recurrence;
+$('#recurrence').on('change', function () {
+    if (this.value === "once" && current_recurrence !== "once") { //Don't do anything if user just clicks same option again
+        current_recurrence = "once";
+        $("#date").html(` <div class="col-md-6"><p>Start date</p><div id="startdate"></div></div><div class="col-md-6 .offset-md-3"><p>End date</p><div id="enddate"></div></div>`);
+        $('#startdate').datetimepicker({
+            format: 'DD/MM/YYYY',
+            inline: true,
+            sideBySide: true
+        });
+        $('#enddate').datetimepicker({
+            format: 'DD/MM/YYYY',
+            inline: true,
+            sideBySide: true,
+            useCurrent: false //Important! See issue #1075
+        });
+        $("#startdate").on("dp.change", function (e) {
+            $('#enddate').data("DateTimePicker").minDate(e.date);
+        });
+        $("#enddate").on("dp.change", function (e) {
+            $('#startdate').data("DateTimePicker").maxDate(e.date);
+        });
+    } else if (this.value === "daily" && current_recurrence !== "daily") { //If
+        current_recurrence = "daily";
+        $("#date").html(null);
+    } else if (this.value === "weekly" && current_recurrence !== "weekly") {
+        current_recurrence = "weekly";
+        $("#date").html(`<label for="recurrence"><p>What day does your event happen on?</p></label><select class="select-css" id="day" name="day"> <option selected disabled>Select</option> <option value="monday">Monday</option> <option value="tuesday">Tuesday</option> <option value="wednesday">Wednesday</option> <option value="thursday">Thursday</option> <option value="friday">Friday</option> <option value="saturday">Saturday</option> <option value="sunday">Sunday</option> </select>`);
+    } else if (this.value === "monthly" && current_recurrence !== "monthly") {
+        current_recurrence = "monthly";
+        $("#date").html(`<label for="week"><p>Which week in the month the event on?</p></label> <select class="select-css" id="week" name="weeknumber"> <option selected disabled>Select</option> <option value="1">Week 1</option> <option value="2">Week 2</option> <option value="3">Week 3</option> <option value="4">Week 4</option> </select><label for="day"><p>What day in the week is the event?</p></label> <select class="select-css" id="day" name="day"> <option selected disabled>Select</option> <option value="Monday">Monday</option> <option value="Tuesday">Tuesday</option> <option value="Wednesday">Wednesday</option> <option value="Thursday">Thursday</option> <option value="Friday">Friday</option> <option value="Saturday">Saturday</option> <option value="Sunday">Sunday</option> </select>`);
+    } else if (this.value === "yearly" && current_recurrence !== "yearly") {
+        $("#date").html(`<label>What month does your event happen on?<select class="form-control" id="month"> <option value="January">January</option> <option value="February">February</option> <option value="March">March</option> <option value="April">April</option> <option value="May">May</option> <option value="June">June</option> <option value="July">July</option> <option value="August">August</option> <option value="September">September</option> <option value="October">October</option> <option value="November">November</option> <option value="December">December</option> </select> </label> <label>What day of the month does your event happen on? <input class="form-control" type="number" min="1" max="31" name="day" id="day"/></label>`);
+
+    }
+});
+
+$('#datetimepicker').datetimepicker({
+    inline: true,
+    sideBySide: true,
+    format: 'LT'
+});
+$('#datetimepicker1').datetimepicker({
+    inline: true,
+    sideBySide: true,
+    format: 'LT'
+});
+$("#datetimepicker").on("dp.change", function (e) {
+    $('#datetimepicker1').data("DateTimePicker").minDate(e.date);
+});
+$("#datetimepicker1").on("dp.change", function (e) {
+    $('#datetimepicker').data("DateTimePicker").maxDate(e.date);
+});
+$("#event_form").submit(function (e) {
+    e.preventDefault();
+    processForm();
+});
