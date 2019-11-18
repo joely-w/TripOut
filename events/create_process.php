5543672bc91e1eb6bd8a37bda28b4549d1b693b0
Joel Winterton <joely-w@users.noreply.github.com>
2019-11-18 17:25:03 +0000

    Calls occurence and location object when data sent

--- events/create_process.php
+++ events/create_process.php
@@ -4,11 +4,13 @@ session_start();
 $Event = new addEvent();
 $generatedEvent = $Event->eventCreate($_POST['eventTitle'], $_SESSION['Username']);
 $element_index = 0;
+$Event->addOccurence($generatedEvent, $_POST['eventOccurence']);
+$Event->addLocation($generatedEvent, $_POST['eventLocation']);
 if ($generatedEvent != false) {
+
     foreach ($_POST['content'] as $section) {
         if ($section['dataType'] == "text" and $Event->checkTags($section['dataSrc'])) { #If content piece is text, and not malicious
-            $insertion = $Event->addContent($generatedEvent, "text", $section['dataSrc'], $element_index);
-            #Add text to database
+            $insertion = $Event->addContent($generatedEvent, "text", $section['dataSrc'], $element_index); #Add text to database
         } elseif ($section['dataType'] == "image") {
             foreach ($section['dataSrc'] as $image_key) {
                 $Event->addContent($generatedEvent, "image", $image_key, $element_index); #Add image to database
@@ -21,3 +23,5 @@ if ($generatedEvent != false) {
 } else {
     #Failure message
 }
+
+
