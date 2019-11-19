41b8fa0cb20966ac7054402e9c845d097c62cd87
Joel Winterton <joely-w@users.noreply.github.com>
2019-11-19 10:58:58 +0000

    Now functions as a callable API via POST instead of an include script

--- events/single_event_process.php
+++ events/single_event_process.php
@@ -1,4 +1,5 @@
 <?
 include_once('/var/www/html/database/config.php'); #Include script, unless there is parent script that has already included
 $EventObject = new Event();
-$Event = $EventObject->displayEvent($_GET['id']); #Gets assoc array with structure: [Datatype, Source]. First element is always title.
+$Event = $EventObject->displayEvent($_POST['eventID']); #Gets assoc array with structure: [Datatype, Source]. First element is always title.
+echo json_encode($Event); #Return information in parsable JSON
