51f3b798069a00d32d230059c0d920d86a49dce4
Joel Winterton <joely-w@users.noreply.github.com>
2019-11-19 10:59:30 +0000

    Now a template that event.js appends to

--- events/event.php
+++ events/event.php
@@ -1,31 +1,20 @@
 <?php
-include("./single_event_process.php");
 $title = $Event[0]["Source"];
+$scripts = array("/images/modal.js", "event.js");
 include('../header.php'); ?>
 <body>
 <?php include('../navigation.php'); ?>
 <div class="container events">
     <div id="events" class="row">
-        <h1><?php echo $title ?></h1>
-        <?php
-        foreach ($Event as $Item) {
-            if ($Item['Datatype'] == "Text") {
-                echo $Item['Source'];
-            } else if ($Item['Datatype'] == "Image") {
-                ?>
-                <div class="event-image">
-                    <ul class="slider">
-                        <li class="visible"> <!-- Current visible slide -->
-                            <img class='event-image' src='<?php echo $Item['Source'] ?>'/>
-                        </li>
+        <h1 id="title"></h1>
+        <div class="modal" id="modal">
+            <a onclick="closeModal()" class="close">&#10006;</a>
+            <img src="" id="modalImage"/>
+        </div>
 
-                </div>
-                <?
-            }
-        }
-        ?>
-        <!-- Events will be appended dynamically here !-->
     </div>
+    <!-- Events will be appended dynamically here !-->
+</div>
 </div>
 <?php
 include('../footer.php'); ?>
