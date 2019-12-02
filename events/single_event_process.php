24331ca0368aa27c3f53032491fa055e4842495f
Joel Winterton <joely-w@users.noreply.github.com>
2019-12-02 23:44:28 +0000

    Now handles multiple request types (popularity and event content)

--- events/single_event_process.php
+++ events/single_event_process.php
@@ -1,5 +1,11 @@
 <?
 include_once('/var/www/html/database/config.php'); #Include script, unless there is parent script that has already included
 $EventObject = new Event();
-$Event = $EventObject->displayEvent($_POST['eventID']); #Gets assoc array with structure: [Datatype, Source]. First element is always title.
-echo json_encode($Event); #Return information in parsable JSON
+if ($_POST['postStatus'] == "event") {
+    $Event = $EventObject->displayEvent($_POST['eventID']); #Gets assoc array with structure: [Datatype, Source]. First element is always title.
+    echo json_encode($Event); #Return information in parsable JSON
+} else if ($_POST['postStatus'] == "popularity") {
+    $LikeStatus = $EventObject->eventPop($_POST['eventID'], $_POST['likeType']);
+    echo json_encode(array("status" => $LikeStatus));
+}
+
