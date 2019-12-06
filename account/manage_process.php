cc961b84f7e9806017172558a6fde8dab9d5807f
Joel Winterton <joely-w@users.noreply.github.com>
2019-12-06 15:20:09 +0000

    Removed special condition for username, as backend generalised enough

--- account/manage_process.php
+++ account/manage_process.php
@@ -3,9 +3,6 @@ include_once('/var/www/html/database/config.php'); #Include script, unless there
 $database = new Login();
 $values = $_POST['data']; #Structure should be: 0: Command (update, destroy or logout), 1: Field name, 2: Field value
 if ($values[0] == "update") { #If values are to be updated execute UpdateField
-    if ($values[1] == "Username") {
-        $_SESSION['OldUsername'] = $_SESSION['Username']; #Backup username in case update fails, in which case revert
-    }
     if ($database->SaveSession($values[1], $values[2]) == true) {
         echo json_encode(array('success' => 1)); #Return success
     } else {
