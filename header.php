802d803d8dcb7aebe948ee5e4bd89ddb051602ca
Joel Winterton <joely-w@users.noreply.github.com>
2019-12-30 22:56:31 +0000

    Changed bootstrap from cdn to local

--- header.php
+++ header.php
@@ -3,7 +3,7 @@ session_start(); #Start session on all pages before headers are sent
 ?>
 <head>
     <title>Trip Out - <?php echo $title ?></title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="/stylesheets/style.css">
