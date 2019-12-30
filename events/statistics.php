c3c2c935a703693da9fa32f4e1ac489818faca91
Joel Winterton <joely-w@users.noreply.github.com>
2019-12-30 22:53:22 +0000

    Added more graphs Added empty vectors for statistics.js appending

--- events/statistics.php
+++ events/statistics.php
@@ -1,27 +1,37 @@
 <?php
-$scripts = array("https://www.gstatic.com/charts/loader.js", "statistics.js");
+$scripts = array("https://www.gstatic.com/charts/loader.js", "statistics.js", "https://d3js.org/d3.v4.js", "https://d3js.org/d3-geo-projection.v2.min.js");
 $styles = array("//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css");
 $title = "Event statistics";
 include('../header.php'); ?>
-<script src="https://www.gstatic.com/charts/loader.js"></script>
-
 <body>
 <?php include('../navigation.php'); ?>
 <div class="container events">
     <div id="statistics">
         <h1 id="title">Your events</h1>
         <select class="form-control" onchange="changeSelect(this.value)" id="selectevents">
-            <option selected disabled>Select event</option>
+            <option value="all" selected>All events</option>
         </select>
-        <div class="charts row">
-            <div class="col-md-6" id="PieChart"></div>
-            <div class="col-md-6" id="LikesPerDay"></div>
-        </div>
-        <div class="row">
-            <div class="col-md-6" id="ViewCounter"></div>
+        <div id="dashboard" class="row dashboard">
+            <div class="col-md-4" id="ViewCounter"></div>
         </div>
     </div>
-    <!-- Events will be appended dynamically here !-->
+    <div class="center" id="filter">
+    </div>
+    <div id="charts" class="charts row">
+        <!-- Could do more, maybe make range dependant on creation of event !-->
+        <div class="col-md-6" id="PieChart">
+            <svg id="my_dataviz" width="440" height="400"></svg>
+        </div>
+        <div class="col-md-6" id="LikesPerDay"></div>
+    </div>
+    <div class="charts row">
+        <div class="col-md-6" id="DislikesPerDay"></div>
+        <div class="col-md-6" id="CumulativePerDay"></div>
+
+    </div>
+</div>
+</div>
+<!-- Events will be appended dynamically here !-->
 </div>
 </div>
 <?php
