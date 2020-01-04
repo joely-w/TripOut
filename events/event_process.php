f911dc7efaa739d022995a2f80dd28e9b7cec28b
Joel Winterton <joely-w@users.noreply.github.com>
2020-01-04 17:31:49 +0000

    Calls appropriate methods if filter is present

--- events/event_process.php
+++ events/event_process.php
@@ -2,8 +2,14 @@
 include_once('/var/www/html/database/config.php'); #Include script, unless there is parent script that has already included
 $Events = new Featured();
 $allSnippets = array();
-foreach ($Events->allEvents() as $row) {
-    $allSnippets[] = ($Events->eventSnippet($row['ID']));
+if (!isset($_POST['filter'])) {
+    foreach ($Events->filterEvent("all") as $row) {
+        $allSnippets[] = ($Events->eventSnippet($row['ID']));
+    }
+} else {
+    foreach ($Events->filterEvent($_POST['conditions']) as $row) {
+        $allSnippets[] = ($Events->eventSnippet($row['ID']));
+    }
 }
 echo json_encode($allSnippets);
 
