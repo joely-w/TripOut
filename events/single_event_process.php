36e190d6f8508af2fede14dec2eb0947996bf511
Joel Winterton <joely-w@users.noreply.github.com>
2020-04-01 12:39:42 +0100

    Processing increased to handle event popularity requests as well as get event data

--- events/single_event_process.php
+++ events/single_event_process.php
@@ -1,11 +1,16 @@
 <?
-include_once('/var/www/html/database/config.php'); #Include script, unless there is parent script that has already included
-$EventObject = new Event();
-if ($_POST['postStatus'] == "event") {
-    $Event = $EventObject->displayEvent($_POST['eventID']); #Gets assoc array with structure: [Datatype, Source]. First element is always title.
-    echo json_encode($Event); #Return information in parsable JSON
-} else if ($_POST['postStatus'] == "popularity") {
-    $LikeStatus = $EventObject->eventPop($_POST['eventID'], $_POST['likeType']);
-    echo json_encode(array("status" => $LikeStatus));
+/**Entirety of single_event_process.php**/
+include_once('../database/config.php'); #Include classes file
+#Instantiate showEvent class and set event ID
+$current_event = new showEvent($_POST['event_id']);
+if ($_POST['post_status'] == "event") { #If user wants to view event
+    #Get event data
+    $event_data = $current_event->displayEvent();
+    #Print event data in JSON
+    echo json_encode(["event_data" => $event_data, "errors" => $current_event->errors]);
+} else if ($_POST['post_status'] == "popularity") { #If user wants to like or dislike an event
+    #Call like method
+    $like_status = $current_event->likeEvent($_POST['like_type']);
+    echo json_encode(["status" => $like_status, "errors" => $current_event->errors]);
 }
 
