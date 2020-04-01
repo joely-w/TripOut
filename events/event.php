11fc4aa42d138df9c7a8fd6ec04ccc425553284c
Joel Winterton <joely-w@users.noreply.github.com>
2020-04-01 12:38:29 +0100

    Structure has been increased to support individual methods appending

--- events/event.php
+++ events/event.php
@@ -1,25 +1,36 @@
 <?php
-$scripts = array("/images/modal.js", "event.js");
+$scripts = array("/images/modal.js", "single_event.js", "//cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js");
 $styles = array("//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css");
 include('../header.php'); ?>
 <body>
-<?php include('../navigation.php'); ?>
-<div class="container events">
-    <div id="events" class="row">
-        <h1 id="title"></h1>
-        <div class="locator container">
-            <div id="occurrence">
-            </div>
-            <div id="map" class="row"></div>
-        </div>
-        <div class="modal" id="modal">
-            <a onclick="closeModal()" class="close">&#10006;</a>
-            <img src="" id="modalImage"/>
-        </div>
 
+<?php include('../navigation.php'); ?>
+<div class="single-event container">
+    <!--Title row-->
+    <div class="row">
+        <h1 id="title"></h1>
     </div>
-    <!-- Events will be appended dynamically here !-->
-</div>
+    <!--Event information row-->
+    <div class="row">
+        <!-- Occurrence, views and like/dislike icons column-->
+        <div class="col-md-6">
+            <div id="occurrence"></div>
+            <div class="views">
+                <i class="fas fa-4x fa-eye"></i>
+                <span id="views"></span>
+            </div>
+            <div id="icons"></div>
+            <div id="address"></div>
+
+        </div>
+        <!--Map container column-->
+        <div class="col-md-6">
+            <div id="map"></div>
+        </div>
+    </div>
+    <div id="event-content" class="row">
+        <!--Event content inserted here-->
+    </div>
 </div>
 <?php
 include('../footer.php'); ?>
