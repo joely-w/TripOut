85a01b9a53af1cbd611471dbe5ba08b1915f5e09
Joel Winterton <joely-w@users.noreply.github.com>
2020-04-01 12:32:39 +0100

    Structure changed User content visibility now uses AJAX instead of inline PHP

--- account/register.php
+++ account/register.php
@@ -2,43 +2,62 @@
 $title = "Register";
 $scripts = array("register.js");
 include('../header.php'); ?>
-
-	<body class="fullscreenpage">
-		<?php include('../navigation.php'); ?>
-		<div class="hero">
-			<ul class="slider">
-				<li id="slider" class="visible">
-					<!-- Static visible slide -->
-					<img alt="Register for TripOut" src="/images/register.jpg">
-					<div class='reg' id="login">
-						<?php if(isset($_SESSION['Username'])){
-	?>
-	<h1 id="register"><?php echo $_SESSION['Username']?>, you're already logged in!</h1>
-
-<?php
-}
-  else{
-						?>
-							<h1 id="register">Register</h1>
-							<div id="errorfield"></div>
-							<!--Content updated if error occurs!-->
-							<form id="regform">
-								<input name='username' placeholder='Username' type='text' />
-
-								<input name='name' placeholder='Full Name' type='text' />
-								<input id='pw' name='password' placeholder='Password' type='password' />
-								<input name='email' placeholder='E-Mail Address' type='text' />
-								<div class='agree'>
-									<input id='agree' name='agree' type='checkbox' />
-									<label for='agree'></label>Accept rules and conditions
-								</div>
-								<input class='animated' type='submit' value='Register' />
-								<a class='forgot' href='#'>Already have an account?</a>
-							</form>
-							<?php }?>
-					</div>
-				</li>
-			</ul>
-
-		</div>
-	</body>
+<body>
+<? include('../navigation.php'); ?>
+<div class="container">
+    <form id="registration_form" class="not_logged_in registration">
+        <!--Will only display form if user not logged in-->
+        <h1>Register</h1>
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3" id="errorfield">
+                <!--Append any errors to this div-->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-md-offset-3">
+                <label>Full name
+                    <input type="text" id="name" placeholder="John Doe"/>
+                </label>
+            </div>
+            <div class="col-md-3"><label>Email
+                    <input type="text" id="email" placeholder="john.doe@example.com"/>
+                </label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <label class="username">Username
+                    <input type="text" id="username" placeholder="JohnDoe20"/>
+                </label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-md-offset-3">
+                <label>Password
+                    <input type="password" id="pwd" placeholder="Password example"/>
+                </label>
+            </div>
+            <div class="col-md-5 passwordstrength" id="passwordstrength">
+                <span class="wrong" id="eight">
+                    &#10006; Password length is less than 8 characters
+                </span>
+                <span class="wrong" id="numbers">
+                    &#10006; Password does not contain any numbers
+                </span>
+                <span class="wrong" id="complexity">
+                    &#10006; Password does not contain one uppercase and one lowercase character
+                </span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2 col-md-offset-5">
+                <button class="btn btn-primary">Register</button>
+            </div>
+        </div>
+    </form>
+    <div class="row logged_in">
+        <h1 class="display_username"><!--Append welcome message to user--></h1>
+    </div>
+</div>
+<? include('../footer.php'); ?>
+</body>
