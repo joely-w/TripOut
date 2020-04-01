c339af9611ae00d9db8b49c9e4a84566cce0370b
Joel Winterton <joely-w@users.noreply.github.com>
2020-04-01 12:40:10 +0100

    Array shorthand now used

--- images/myImages.php
+++ images/myImages.php
@@ -1,10 +1,10 @@
 <?
-include_once('/var/www/html/database/config.php'); #Include script, unless there is parent script that has already included
-$Image = new MyImages();
+include_once('../database/config.php'); #Include script, unless there is parent script that has already included
+$Image = new Images();
 $images_arr = $Image->DisplayImages($_SESSION['Username']);
-$full_arr = array();
+$full_arr = [];
 foreach ($images_arr as $image) {#Structure: Filename, Filetype
     $path = "/events/images/" . $_SESSION['Username'] . "/" . $image['Filename'] . "." . $image['Filetype'];
-    $full_arr[] = array($path, $image['FileID']);
+    $full_arr[] = [$path, $image['FileID']];
 }
 echo json_encode($full_arr);
