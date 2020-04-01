31a21f4cf4638ea6ed2b130b8c3c0af5f55cdca7
Joel Winterton <joely-w@users.noreply.github.com>
2020-04-01 12:41:47 +0100

    Completely changed to OOP All global variables have been eliminated

--- hero.js
+++ hero.js
@@ -1,28 +1,57 @@
-var int_slide = 0; //Set current slide index
-var list_slides = document.querySelectorAll('ul.slider li'); //Get a list which has a new element for each slide
-var int_size = list_slides.length;
+class Hero {
+    constructor() {
+        //Set current slide index
+        this.current_slide = 0;
 
-function updateSlide(number) {
-    list_slides[int_slide].classList.remove('visible'); //Remove current visible slide
-    if (number > int_size - 1) { //If at the end of slides, reset to beginning//
-        int_slide = 0
-    } else if (number < 0) {
-        int_slide = int_size - 1;
-    } else { //Otherwise go to the next slide in list//
-        int_slide = number;
+        //Get a list of the slides available
+        this.all_slides = document.querySelectorAll('ul.slider li');
+
+        //Set the number of slides
+        this.maximum_slides = this.all_slides.length;
     }
-    list_slides[int_slide].classList.add('visible'); //Make the new slide visible
 
+    updateSlide(slide_index) {
+
+        //Remove current visible slide
+        this.all_slides[this.current_slide].classList.remove('visible');
+        //If at the end of slides
+        if (slide_index > this.maximum_slides - 1) {
+            //Reset to beginning
+            this.current_slide = 0
+        }
+        //If going left at beginning of slides
+        else if (slide_index < 0) {
+            //Go to end of slides
+            this.current_slide = this.maximum_slides - 1;
+        }
+        //If not at beginning or end
+        else {
+            //Change slide to passed index
+            this.current_slide = slide_index;
+        }
+
+        //Make the new slide visible
+        this.all_slides[this.current_slide].classList.add('visible');
+    }
 }
 
-function autoUpdate() {
-    updateSlide(int_slide + 1); //Used for setInterval
-}
-
-setInterval(autoUpdate, 10000);
-document.getElementById('prev').addEventListener('click', function () { //If previous button is clicked, go to previous slide//
-    updateSlide(int_slide - 1)
-});
-document.getElementById('next').addEventListener('click', function () {//If next button is clicked, go to next slide//
-    updateSlide(int_slide + 1)
+//When the DOM is ready, execute anonymous function
+$(document).ready(function () {
+    //Instantiate class
+    let slider = new Hero();
+    //Execute function every ten seconds
+    setInterval(function () {
+        //Go to next slider in hero slider
+        slider.updateSlide(slider.current_slide + 1)
+    }, 10000);
+    //If previous button clicked
+    $("#prev").on('click', function () {
+        //Go to previous slider in hero slider
+        slider.updateSlide(slider.current_slide - 1)
+    });
+    //If next button is clicked
+    $("#next").on('click', function () {
+        //Go to next slider in hero slider
+        slider.updateSlide(slider.current_slide + 1)
+    })
 });
