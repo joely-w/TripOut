92b6e56ec0d001e8fae91b2d793ff35bed667bff
Joel Winterton <joely-w@users.noreply.github.com>
2020-04-01 12:40:43 +0100

    Username no longer returned with image upload

--- images/upload_image_process.php
+++ images/upload_image_process.php
@@ -1,13 +1,12 @@
 <?
-/**Upload image REST API**/
-include_once('/var/www/html/database/config.php'); #Include script, unless there is parent script that has already included
-$database = new myImages();
-$upload = $database->UploadImage($_FILES["file"], $_SESSION['Username']);
-if ($upload[0]==true) { #First array item will be success boolean, second will be file path
-    echo json_encode(array('success' => 1, 'Username' => $_SESSION['Username'], 'Filepath' => $upload[1], 'errors' => $database->errors));
+/**Entirety of upload_image_process.php**/
+include_once('../database/config.php'); #Include classes file
+$database = new Images();
+$upload = $database->UploadImage($_FILES["image"]); //Get upload file
+if ($upload['success'] == true) {#If file validates and uploads properly
+    echo json_encode(['success' => 1]); #Report success
+
+} else {#If upload fails
+    echo json_encode(['success' => 0, 'errors' => $database->errors]);  #Report failure along with errors
 
 }
-else{
-    echo json_encode(array('success' => 0, 'errors' => $database->errors));
-
-};
